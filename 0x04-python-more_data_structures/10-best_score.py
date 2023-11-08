a95cad0e02eba017d83a95e90efaa8bb532a852d
Saif <saifeddine.belhaj@gmail.com>
2023-11-08 21:03:14 +0100

    job done

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    is_first = True
+    big = None
+    for key in a_dictionary:
+        if is_first is True:
+            is_first = False
+            big = key
+        else:
+            if a_dictionary[key] > a_dictionary[big]:
+                big = key
+    return big
---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+
+def roman_to_int(roman_string):
+    if roman_string is None or isinstance(roman_string, str) is not True:
+        return 0
+    dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                  'D': 500, 'M': 1000}
+    total = 0
+    prev = 0
+    for let in roman_string:
+        if let not in dictionary:
+            return 0
+        if dictionary[let] > total:
+            total = dictionary[let] - total
+        elif dictionary[let] > prev:
+            total = total + dictionary[let] - prev * 2
+        else:
+            total = dictionary[let] + total
+        prev = dictionary[let]
+    return total
