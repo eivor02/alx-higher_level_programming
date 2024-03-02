59e6302b3c7a0d35e04557445fe4da8ac88fd9d7
Saif <saifeddine.belhaj@gmail.com>
2024-03-02 12:36:12 +0100

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-'''
-fetches a webpage
-'''
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
 
-from urllib.request import urlopen
 
+if __name__ == '__main__':
+    import urllib.request
 
-if __name__ == "__main__":
-        with urlopen('https://intranet.hbtn.io/status') as response:
-                message = response.read()
-                print("Body response:")
-                print('\t- type: {}'.format(message.__class__))
-                print('\t- content: {}'.format(message))
-                print('\t- utf8 content: {}'.format(message.decode('utf-8')))
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
